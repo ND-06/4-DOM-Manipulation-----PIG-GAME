426ccda15cc4b962ab7b4d85895872e5473b9288
Natdenice <Clad1987@hotmail.com>
2019-01-08 16:38:39 +0100

    event listener for roll dice added

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ GAME RULES:
 
 // We start to create the most important variables for our game
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 scores = [0,0];
 roundScore = 0; 
 activePlayer = 1; // we need to write 0 cause we will access scores array 
 // through activeplayer , and stores array begins at the index 0.
 
-dice = Math.floor(Math.random() * 6) + 1;
+
 /* we want a random number between 1 and 6 , for that , we assign at dice variable
 the Math method , Math.random will give us a number between 0 and 1 by default,
 that is why we multiply the result by 6 , so the number given will be between
@@ -34,7 +34,7 @@ DOM is the document object.
  -- 2 WAYS TO INTERACT WITH THE CONTENT >>> textContent and innerHtml
 */
 
-document.querySelector('#current-' + activePlayer).textContent = dice; // One of the most used selector to manipulate
+// document.querySelector('#current-' + activePlayer).textContent = dice; // One of the most used selector to manipulate
                            // DOM is the querySelector. He only select the first element that it finds
                            // but there is a trick to avoid that It is the same that select a class in CSS
                            // We need a string inside the selector and then
@@ -53,26 +53,83 @@ document.querySelector('#current-' + activePlayer).textContent = dice; // One of
 /* We can also use query.selector and textContent to read only */
 // for this, we can assign to a variable the method document.querySelector
 
-var x = document.querySelector('#score-0').textContent;
+// var x = document.querySelector('#score-0').textContent;
 // we call this a GETTER cause with this textContent in this context , we read a value
-console.log(x); // it will display the total score of player 1 in the js console
+// console.log(x); // it will display the total score of player 1 in the js console
 
 
 /* WE can also use querySelector to change the css of some elements
 */
 // for example, we want to hide the display of the dice (class > dice)
 
-//document.querySelector('.dice').style.display = 'none';
+  document.querySelector('.dice').style.display = 'none';
 // we use the querySelector , in the parenthesis, we put , into a string the class name
 // (with a .) or the id name (with a #) and then we put 'style' ( to interact with css) + 
 // the property that we want to change (display) and we assign to the value of the property 
 // ( 'none' in our case)
 
-function btn() {
-  //do something here
-}
 
-document.querySelector('.btn-roll').addEventListener('click', btn);
+
+// *** EVENTS AND EVENTS HANDLING ***
+
+// The first step is to select the element related to the event that we want to set
+// for this we can use querySelector, or getElementByID 
+// For us, we want to display the good number of the dice , so we will
+// interact with the .btn-roll class
+// then we have to add addEventListener , and we have two arguments
+// the FIRST one is the event type , and for us it will be 'click'
+// but there are a lot of different events types (Event reference /mdn )
+// the SECOND argument will be the function that will be executed as soon
+// as the event happens. We have 2 possibilities : create a function outside 
+// document.queryselector ( with no parameters ) or inside document.querySelector ( anonymous functions)
+// and in this case , we will not be able to use this anonymous function outside
+// the event. In an anonymous function, we can write just after the first argument in our event, 
+// the complete function ( this anonymous function will not have a name like a default function)
+// An anonymous function cannot be reused and have not name
+ 
+
+// btn(); // if we want to call this function, by default we write it like this
+
+// function declared outside the event but used in event ( in 2d argument, always without parenthesis)
+// we dont write parenthesis after btn cause we want that the click event launch the function
+// it is called the CALLBACK FUNCTION --- The function is not called by us but by the event or
+// by another function
+
+// This is a default function
+// function btn() {
+// console.log("hello");
+ // }
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+  // 1. Random number
+  var dice = Math.floor(Math.random() * 6) + 1;
+  // 2.Display the result
+  document.querySelector('.dice').style.display = 'block';
+  
+  // we want to set all scores to 0
+
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  // we can use getElementByID when we have to interact with ID , it will be faster
+  // than the querySelector, and we dont have to put a # in parenthesis like querySelector
+  // we create a variable  where we store the selection and then we 
+  // can use this variable whenever we need it ( inside the scope)
+  var diceDom =  document.querySelector('.dice');
+  diceDom.style.display = "block";
+  // we can change the image depending to the result of the dice by
+  // just adding src equal to the name of the image.
+  diceDom.src = 'dice-' + dice + '.png';
+  // 3.Update the roundScore but only IF the rolled number was not 1
+  if (dice != 1) {
+    //add score
+  } else {
+    // next player
+  }
+
+});
 
 
 
